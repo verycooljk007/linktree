5d16d90ee8b9890b60a4ccb97f671260a55484b4
Meshaal Khan <62823214+verycooljk007@users.noreply.github.com>
2025-12-04 15:06:52 -0500

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,24 @@
+class CustomFooter extends HTMLElement {
+    connectedCallback() {
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+            <style>
+                .footer {
+                    margin-top: 2.5rem;
+                    text-align: center;
+                    font-size: 0.625rem;
+                    color: #666;
+                    border-top: 1px solid #333;
+                    padding-top: 0.625rem;
+                    font-family: 'Courier New', Courier, monospace;
+                }
+            </style>
+            <div class="footer">
+                <p>:: CASTLEMORE ROAD STRATEGIES © ${new Date().getFullYear()} :: BRAMPTON, ON ::</p>
+                <p>DATA DELAYED 15 MIN</p>
+            </div>
+        `;
+    }
+}
+
+customElements.define('custom-footer', CustomFooter);
